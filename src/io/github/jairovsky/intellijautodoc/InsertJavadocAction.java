b299a62482da161ec2bb6caf0e482196da1255e8
Jairo Santos <jairo.santos@payu.com.br>
2018-05-29 18:04:01 -0300

    Revert "WIP: execute the action only in the caret context. The caret context can be inside a method, inside a class, or the entire file."
    This reverts commit 195b3f18dc32bb8b8ca496120f14a7d22d57e280.

--- src/io/github/jairovsky/intellijautodoc/InsertJavadocAction.java
+++ src/io/github/jairovsky/intellijautodoc/InsertJavadocAction.java
@@ -2,36 +2,23 @@ package io.github.jairovsky.intellijautodoc;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.util.Condition;
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
-
-import static java.lang.String.format;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
 
 public class InsertJavadocAction extends HideableAction {
-
-    private static final Logger logger = Logger.getInstance(InsertJavadocAction.class);
 
     @Override
     public void actionPerformed(AnActionEvent event) {
 
         PsiJavaFile file = (PsiJavaFile) event.getData(LangDataKeys.PSI_FILE);
-        Integer caretPosition = event.getData(LangDataKeys.CARET).getOffset();
 
-        PsiElement currentFocusedElement = file.findElementAt(caretPosition);
+        if (file != null) {
+            UndocumentedMethodVisitor methodVisitor = new UndocumentedMethodVisitor(file);
+            UndocumentedClassVisitor classVisitor = new UndocumentedClassVisitor(file);
 
-        PsiElement methodOrClass = (currentFocusedElement instanceof PsiMethod) ? (PsiMethod) currentFocusedElement :
-                PsiTreeUtil.getParentOfType(currentFocusedElement, PsiMethod.class);
-
-        methodOrClass = (methodOrClass != null) ? methodOrClass :
-                PsiTreeUtil.getParentOfType(currentFocusedElement, PsiClass.class);
-
-        UndocumentedMethodVisitor methodVisitor = new UndocumentedMethodVisitor(file);
-        UndocumentedClassVisitor classVisitor = new UndocumentedClassVisitor(file);
-
-        new BatchWriteAction(file.getProject(), classVisitor, methodVisitor).execute();
+            new BatchWriteAction(file.getProject(), classVisitor, methodVisitor).execute();
+        }
     }
 
     @Override
