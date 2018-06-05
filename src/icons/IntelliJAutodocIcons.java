80f60f2e446b278cb1a055a2088256251565a55e
Jairo S <jairosantos08@gmail.com>
2018-06-05 00:41:00 -0300

    add action icon

---
+++ src/icons/IntelliJAutodocIcons.java
@@ -0,0 +1,10 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface IntelliJAutodocIcons {
+
+    Icon INSERT_JAVADOC = IconLoader.getIcon("/icons/coffee-cup.png");
+}
